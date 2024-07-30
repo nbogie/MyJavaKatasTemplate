5577b79fc344534de36b5d9a63e8169676cb5e5f
nbogie <neillbogie@googlemail.com>
2024-07-30 16:26:33 +0100

    Initial commit

---
+++ src/test/java/org/neill/katas/sumofpositive/MainSumOfPositiveTest.java
@@ -0,0 +1,10 @@
+package org.neill.katas.sumofpositive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainSumOfPositiveTest {
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+    }
+}
